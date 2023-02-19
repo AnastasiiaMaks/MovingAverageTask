b2c68c5c0c5aa935fa62096af222ef5cdd50c333
Anastasiia Maks <anastasiia0188@gmail.com>
2023-02-19 17:28:04 +0300

    Testing moving_average function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <numeric>
+
 
 std::vector<double> generate_data_double(int data_length)
 // generates vector of double 
 {
 	std::vector<double> data;
-	for(int i=0; i < data_length; i++)
+	for(int i = 0; i < data_length; i++)
 	{
-		data.push_back(rand());
+		data.push_back(rand() % 10);
 	}
 	return data;
 }
@@ -17,13 +19,35 @@ std::vector<float> generate_data_float(int data_length)
 // generates vector of float 
 {
 	std::vector<float> data;
-	for(int i=0; i < data_length; i++)
+	for(int i = 0; i < data_length; i++)
 	{
-		data.push_back(rand());
+		data.push_back(rand() % 10);
 	}
 	return data;
 }
 
+std::vector<float> moving_average_float(std::vector<float> &data, int window_size)
+// calculates vector of moving average float 
+{
+	std::vector<float> mov_aver;
+
+	// write first value
+	mov_aver.push_back(data[0]);
+
+	for(int i = 1; i < data.size(); i++)
+	{	
+		// start case 
+		if((i - window_size)<0)
+		{
+			mov_aver.push_back(std::accumulate(data.cbegin(), data.cbegin() + i + 1, 0)/(i+1));
+		} else
+		// main case
+		{
+			mov_aver.push_back(std::accumulate(data.cbegin() + (i - window_size + 1), data.cbegin() + i + 1, 0)/window_size);
+		}		
+	}
+	return mov_aver;
+}
 
 int main()
 {
@@ -52,6 +76,18 @@ int main()
 	for(int i=0; i < data_length; i++)
 	{
 		std::cout << data_double[i] << std::endl;
+	}
+	
+	// window size
+	int window_size = 4;
+
+	// calculate moving average float
+	std::vector<float> moving_average = moving_average_float(data_float, window_size);
+
+	std::cout << "Float moving average: " << std::endl;
+	for(int i=0; i < data_length; i++)
+	{
+		std::cout << moving_average[i] << std::endl;
 	}
 
 	return 0;
