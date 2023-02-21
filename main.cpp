80a94c84e37c40d629559b97c303915b73e0917d
Anastasiia Maks <anastasiia0188@gmail.com>
2023-02-21 20:01:19 +0300

    Add functions for float and double

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ std::vector<double> generate_data_double(int data_length)
 	std::vector<double> data;
 	for(int i = 0; i < data_length; i++)
 	{
-		data.push_back(rand() % 10);
+		data.push_back(rand());
 	}
 	return data;
 }
@@ -22,7 +22,7 @@ std::vector<float> generate_data_float(int data_length)
 	std::vector<float> data;
 	for(int i = 0; i < data_length; i++)
 	{
-		data.push_back(rand() % 10);
+		data.push_back(rand());
 	}
 	return data;
 }
@@ -50,12 +50,39 @@ std::vector<float> moving_average_float(std::vector<float> &data, int window_siz
 	return mov_aver;
 }
 
+std::vector<double> moving_average_double(std::vector<double> &data, int window_size)
+// calculates vector of moving average double 
+{
+	std::vector<double> mov_aver;
+
+	// write first value
+	mov_aver.push_back(data[0]);
+
+	for(int i = 1; i < data.size(); i++)
+	{	
+		// start case 
+		if((i - window_size)<0)
+		{
+			mov_aver.push_back(std::accumulate(data.cbegin(), data.cbegin() + i + 1, 0)/(i+1));
+		} else
+		// main case
+		{
+			mov_aver.push_back(std::accumulate(data.cbegin() + (i - window_size + 1), data.cbegin() + i + 1, 0)/window_size);
+		}		
+	}
+	return mov_aver;
+}
+
+
 int main()
 {
 	// inter initial data
-	int data_length;
+	int data_length;        // data array length	
+	int window_size;        // window size
 	std::cout << "Inter length of data: " << std::endl;
 	std::cin >> data_length ;
+	std::cout << "Inter length of window: " << std::endl;
+	std::cin >> window_size;
 
 	// starting point
 	srand(std::time(NULL));
@@ -78,19 +105,28 @@ int main()
 	//{
 	//	std::cout << data_double[i] << std::endl;
 	//}
-	
-	// window size
-	int window_size = 4;
-	
-	// start time measuring
-	const auto t_start = std::chrono::steady_clock::now();
+		
+	// start time measuring 1
+	auto t_start = std::chrono::steady_clock::now();
 
 	// calculate moving average float
-	std::vector<float> moving_average = moving_average_float(data_float, window_size);
+	std::vector<float> moving_average_f = moving_average_float(data_float, window_size);
 	
-	// calculate time
-	const auto dt = std::chrono::steady_clock::now() - t_start;
-      std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(dt).count() << " ms" << std::endl;
+	// calculate time 1
+	auto dt = std::chrono::steady_clock::now() - t_start;
+      std::cout << "Time for FLOAT type: " << std::chrono::duration_cast<std::chrono::microseconds>(dt).count() << " micros" << std::endl;
+
+	// start time measuring 2
+	t_start = std::chrono::steady_clock::now();
+
+	// calculate moving average double
+	std::vector<double> moving_average_d = moving_average_double(data_double, window_size);
+	
+	// calculate time 2
+	dt = std::chrono::steady_clock::now() - t_start;
+      std::cout << "Time for DOUBLE type: " << std::chrono::duration_cast<std::chrono::microseconds>(dt).count() << " micros" << std::endl;
+
+	
 
 	//std::cout << "Float moving average: " << std::endl;
 	//for(int i=0; i < data_length; i++)
